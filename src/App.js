6be4ebb4958be5924ea6a903b16b35ea428498dd
Navjot Singh <navjot2608@gmail.com>
2020-10-17 16:58:32 -0400

    component changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,81 @@
 import React from "react";
 import data from "./data.json";
 import Products from "./components/Products";
+import Filter from "./components/Filter";
+import Cart from "./components/Cart";
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       products: data.products,
+      cartItems: [],
       size: "",
       sort: ""
     };
   }
+
+  removeFromCart = (product) => {
+    const cartItems = this.state.cartItems;
+    this.setState({
+      cartItems: cartItems.filter((x) => x._id !== product._id)
+    });
+  };
+
+  addToCart = (product) => {
+    const cartItems = this.state.cartItems;
+    let alreadyInCart = false;
+    cartItems.forEach((item) => {
+      if (item._id === product._id) {
+        item.count++;
+        alreadyInCart = true;
+      }
+    });
+    if (!alreadyInCart) {
+      cartItems.push({ ...product, count: 1 });
+    }
+    this.setState({ cartItems });
+  };
+
+  sortProduct = (event) => {
+    const sort = event.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id > b._id
+            ? 1
+            : -1
+        )
+    }));
+  };
+
+  filterProduct = (event) => {
+    const value = event.target.value;
+    console.log(event.target.value);
+    if (value === "") {
+      this.setState({
+        size: value,
+        products: data.products
+      });
+    } else {
+      this.setState({
+        size: value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(value) >= 0
+        )
+      });
+    }
+  };
 
   render() {
     return (
@@ -21,9 +86,24 @@ class App extends React.Component {
         <main>
           <div className="content">
             <div className="main">
-              <Products products={this.state.products} />
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProduct={this.filterProduct}
+                sortProduct={this.sortProduct}
+              />
+              <Products
+                products={this.state.products}
+                addToCart={this.addToCart}
+              />
             </div>
-            <div className="sidebar">Cart Items</div>
+            <div className="sidebar">
+              <Cart
+                cartItems={this.state.cartItems}
+                removeFromCart={this.removeFromCart}
+              />
+            </div>
           </div>
         </main>
         <footer>All right is reserved.</footer>
